b7af70736199e8627fcf064df9c46948246b56be
Ernz-Perkd <ernz@perkd.me>
2024-03-04 16:24:11 +0800

    update

--- app.js
+++ app.js
@@ -1,47 +1,58 @@
 const express = require('express');
 const process = require('process');
 const app = express();
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 
+let totalRequestedMemory = []
 let bufferArray = []
 
 app.get('/allocate', (req, res) => {
   // Receive the desired memory allocation 
   const requestedMemory = parseInt(req.query.memory, 10) || 0
+  const requestedCount = parseInt(req.query.count, 10) || 1
   // Allocate memory in MB
   const BUFFER_SIZE = requestedMemory * 1024 * 1024;
-  const allocationDelay = 1000
 
   const allocateMemory = () => {
-    console.log('Memory Buffer in MB: ' + (BUFFER_SIZE))
-    setTimeout(() => {
+    console.log('Memory Buffer in MB: ' + (BUFFER_SIZE * requestedCount))
 
+    for (let i = 0; i < requestedCount; i++) {
       bufferArray.push(Buffer.alloc(BUFFER_SIZE))
-      bufferArray[bufferArray.length - 1].fill(0)
-      console.log(`Buffer array length: ${bufferArray.length}`)
-      
-      console.log('Memory released');
-    }, allocationDelay);
+      const bufferAt = (bufferArray.length - 1)
+      bufferArray[bufferAt].fill(1)
+      totalRequestedMemory.push(requestedMemory)
+    }
+    console.log(`BUFFER ARRAY: ${bufferArray.length}`)
+    console.log(`Total requested memory: ${totalRequestedMemory.reduce((accumulator, currentValue) => accumulator + currentValue, 0)}`)
+
   };
 
+  // Simulate memory allocation in a loop
   allocateMemory();
+
   res.send(`Memory allocated: ${(BUFFER_SIZE)}`);
 });
 
 // Deallocate latest added memory buffer
 app.get('/deallocate', (req, res) => {
+  const requestedCount = parseInt(req.query.count, 10) || 1
   const deallocateMemory = () => {
-    bufferArray.pop()
+    for (let i = 0; i < requestedCount; i++) {
+      bufferArray.pop()
+      totalRequestedMemory.pop()
+    }
     global.gc()
   }
   deallocateMemory()
+  console.log(`Total requested memory: ${totalRequestedMemory.reduce((accumulator, currentValue) => accumulator + currentValue, 0)}`)
   res.send(`Deallocated memory`)
 })
 
-// Clear all memory buffers
 app.get('/clear', (req, res) => {
   const clearMemory = () => {
-    bufferArray = null
+    bufferArray = []
+    totalRequestedMemory = []
+    console.log(`Total requested memory: ${totalRequestedMemory.reduce((accumulator, currentValue) => accumulator + currentValue, 0)}`)
     global.gc()
   }
   clearMemory()
